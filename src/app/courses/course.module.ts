468d7812c3bb75b292418c2383fe614110b572fd
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-14 11:58:32 -0300

    Shared module

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -1,22 +1,22 @@
-import { StarComponent } from './../star/star.component';
-import { ReplacePipe } from './../pipe/replace.pipe';
 import { CourseListComponent } from './course-list.component';
 import { RouterModule } from '@angular/router';
 import { CourseInfoComponent } from './course-info.component';
 import { NgModule } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { StarModule } from '../shared/component/star/star.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 
 @NgModule({
   declarations: [
     CourseListComponent,
     CourseInfoComponent,
-    StarComponent,
-    ReplacePipe
   ],
   imports: [
     CommonModule,
     FormsModule,
+    StarModule,
+    AppPipeModule,
     RouterModule.forChild([
       {
         path: "courses", component: CourseListComponent
