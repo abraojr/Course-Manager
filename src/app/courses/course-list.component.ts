bf4df9e2e9e5d7ab9d74147d1f92c20c7d07e290
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-14 11:09:34 -0300

    Creating course service and using dependency injection

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
@@ -9,28 +10,11 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {
+  }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: "Angular: Forms",
-        imageUrl: "/assets/images/forms.png",
-        price: 99.99,
-        code: "XPS-8796",
-        duration: 120,
-        rating: 4.5,
-        releaseDate: "November, 2, 2021"
-      },
-      {
-        id: 2,
-        name: "Angular: HTTP",
-        imageUrl: "/assets/images/http.png",
-        price: 45.99,
-        code: "LKL-1094",
-        duration: 120,
-        rating: 4,
-        releaseDate: "December, 4, 2021"
-      }
-    ]
+    this.courses = this.courseService.retrieveAll();
+
   }
 }
