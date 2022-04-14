7d0d04bdb1b821f0c50c2d14fd0da6709c3c5362
Abrão Astério Junior <abcorinthians@gmail.com>
2022-04-14 11:20:34 -0300

    Understanding Routes and Protecting Routes with Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
@@ -12,11 +15,24 @@ import { ReplacePipe } from './pipe/replace.pipe';
     AppComponent,
     CourseListComponent,
     StarComponent,
+    NavBarComponent,
+    Error404Component,
     ReplacePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: "", redirectTo: "courses", pathMatch: "full"
+      },
+      {
+        path: "courses", component: CourseListComponent
+      },
+      {
+        path: "**", component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
